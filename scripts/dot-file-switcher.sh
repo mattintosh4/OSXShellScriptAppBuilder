54464841eef1388d01b094186419c93fcc8b2458
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 04:05:17 +0900

    .

--- scripts/dot-file-switcher.sh
+++ scripts/dot-file-switcher.sh
@@ -5,6 +5,8 @@ FILE=/tmp/tmp_$$
 PREF="com.apple.finder"
 PROP="AppleShowAllFiles"
 cat > $FILE <<__EOF__
+#!/bin/bash
+
 case `defaults read $PREF $PROP` in
 	0) defaults write $PREF $PROP -bool ON;;
 	1) defautls write $PREF $PROP -bool OFF;;
