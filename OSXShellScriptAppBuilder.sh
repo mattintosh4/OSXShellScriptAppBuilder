3053c5266a6c6450e01a1fbd6bf74a30174319fe
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 13:09:38 +0900

    .

--- OSXShellScriptAppBuilder.sh
+++ OSXShellScriptAppBuilder.sh
@@ -19,7 +19,7 @@ more info:
 __EOF__
 
 echo "=> モジュールセットをダウンロードしています。"
-. <(curl https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/moduleset)
+. <(curl https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/moduleset.sh)
 
 
 echo "=> モジュールをダウンロードしています。"
