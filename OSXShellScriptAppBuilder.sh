2698a4ad53e505e975890f9a69dfe7f4addedd18
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 14:49:39 +0900

    .

--- OSXShellScriptAppBuilder.sh
+++ OSXShellScriptAppBuilder.sh
@@ -16,7 +16,7 @@ __EOF__
 
 echo "=> モジュールをダウンロードしています。"
 DL_MODULE(){
-	MODULE_LOCALE="https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules/$1.sh"
+	MODULE_LOCALE="https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules/${1}.sh"
 	MODULE_FILE=/tmp/tmp_$$
 	echo "$MODULE_LOCALE => $MODULE_FILE"
 	curl --progress-bar -o $MODULE_FILE $MODULE_LOCALE
