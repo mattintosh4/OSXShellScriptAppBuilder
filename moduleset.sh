875daa14244cf50f10f5a947d3bc82823d56e803
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 13:25:14 +0900

    .

--- moduleset.sh
+++ moduleset.sh
@@ -1,5 +1,7 @@
 LOCALE_PREFIX='https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules'
-case "$1" in
+MODULE_NAME="$1"
+echo $MODULE_NAME
+case "$MODULE_NAME" in
 	wine-explorer				) MODULE_LOCALE="$LOCALE_PREFIX/wine-explorer.sh";;
 	macports-active-list-backup	) MODULE_LOCALE="$LOCALE_PREFIX/macports-active-list-backup.sh";;
 	dot-file-switcher			) MODULE_LOCALE="$LOCALE_PREFIX/dot-file-switcher.sh";;
