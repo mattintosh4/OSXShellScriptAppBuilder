484fe2566e3672900b95ef25c4a11bb9c43507d2
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 14:33:16 +0900

    .

--- moduleset.sh
+++ moduleset.sh
@@ -1,16 +1,6 @@
-MODULE_PATH='https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules'
-case "$1" in
-	"dot-file-switcher")
-		MODULE_PATH="$MODULE_PATH/dot-file-switcher.sh";;
-	"macports-active-list-backup")
-		MODULE_PATH="$MODULE_PATH/macports-active-list-backup.sh";;
-	"wine-explorer")
-		MODULE_PATH="$MODULE_PATH/wine-explorer.sh";;
-	*)
-		echo "モジュールが見つかりませんでした"; exit;;
-esac
+MODULE_PATH="https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules/$1.sh"
 echo "=> モジュールをダウンロードしています。"
 if ! curl --progress-bar -o "$2" "$MODULE_PATH"; then
-	echo "スクリプトのダウンロードに失敗しました。処理を中止します。"
+	echo "モジュールのダウンロードに失敗しました。モジュール名が正しいか確認して下さい。処理を中止します。"
 	exit
 fi
