fed170603720a13fc2c1dbadb4fb6e7b476395fe
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 14:27:38 +0900

    .

--- moduleset.sh
+++ moduleset.sh
@@ -1,9 +1,13 @@
 MODULE_PATH='https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules'
 case "$1" in
-	"dot-file-switcher"				) MODULE_PATH="$MODULE_PATH/dot-file-switcher.sh";;
-	"macports-active-list-backup"	) MODULE_PATH="$MODULE_PATH/macports-active-list-backup.sh";;
-	"wine-explorer"					) MODULE_PATH="$MODULE_PATH/wine-explorer.sh";;
-	*								) echo "モジュールが見つかりませんでした"; exit;;
+	"dot-file-switcher")
+		MODULE_PATH="$MODULE_PATH/dot-file-switcher.sh";;
+	"macports-active-list-backup")
+		MODULE_PATH="$MODULE_PATH/macports-active-list-backup.sh";;
+	"wine-explorer")
+		MODULE_PATH="$MODULE_PATH/wine-explorer.sh";;
+	*)
+		echo "モジュールが見つかりませんでした"; exit;;
 esac
 echo "=> モジュールをダウンロードしています。"
 if ! curl --progress-bar -o "$2" "$MODULE_PATH"; then
