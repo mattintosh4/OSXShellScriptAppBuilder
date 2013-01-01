06093f10ed889e4be7c28af06edc4cc25bdbbe16
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 10:11:24 +0900

    .

--- modules/dot-file-switcher.sh
+++ modules/dot-file-switcher.sh
@@ -4,12 +4,11 @@
 FILE=/tmp/tmp_$$
 cat > $FILE <<__EOF__
 #!/bin/bash
-
+echo "Finder を再起動します。よろしければ Enter を押して下さい。"
+read
 (( `defaults read com.apple.finder AppleShowAllFiles` == 0 )) \
 	&& defaults write com.apple.finder AppleShowAllFiles -bool TRUE \
 	|| defaults write com.apple.finder AppleShowAllFiles -bool FALSE
-echo "Finder を再起動します。よろしければ Enter を押して下さい。"
-read
 killall Finder
 __EOF__
 chmod +x $FILE
