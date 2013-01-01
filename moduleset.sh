cdcd5fcf8fc26b948dedbbf4c5a80caf1d1e3a12
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 13:22:55 +0900

    .

--- moduleset.sh
+++ moduleset.sh
@@ -1,11 +1,11 @@
-MODULE_PREFIX="https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules"
+LOCALE_PREFIX='https://raw.github.com/mattintosh4/OSXShellScriptAppBuilder/master/modules'
+
+echo "$1"
+
 case "$1" in
-	wine-explorer)
-		MODULE="$MODULE_PREFIX/wine-explorer.sh";;
-	macports-active-list-backup)
-		MODULE="$MODULE_PREFIX/macports-active-list-backup.sh";;
-	dot-file-switcher)
-		MODULE="$MODULE_PREFIX/dot-file-switcher.sh";;
-	*) echo "モジュールが見つかりませんでした"; exit;;
+	wine-explorer				) MODULE_LOCALE="$LOCALE_PREFIX/wine-explorer.sh";;
+	macports-active-list-backup	) MODULE_LOCALE="$LOCALE_PREFIX/macports-active-list-backup.sh";;
+	dot-file-switcher			) MODULE_LOCALE="$LOCALE_PREFIX/dot-file-switcher.sh";;
+	*							) echo "モジュールが見つかりませんでした"; exit;;
 esac
-export MODULE
+export MODULE_LOCALE
