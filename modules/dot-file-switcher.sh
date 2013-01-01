c3f300ff481a63ebe01a6771a2acbdf0c3a61df4
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 14:20:54 +0900

    .

--- modules/dot-file-switcher.sh
+++ modules/dot-file-switcher.sh
@@ -4,7 +4,7 @@
 FILE=/tmp/tmp_$$
 cat > $FILE <<__EOF__
 #!/bin/bash
-echo "ドットファイル表示切替のため Finder を再起動します。よろしければ Enter を押して下さい。"
+echo "ドットファイル表示切替のため Finder を再起動します。よろしければ Enter を押して下さい。キャンセルする場合は C-c を押して下さい。"
 read
 (( `defaults read com.apple.finder AppleShowAllFiles` == 0 )) \
 	&& defaults write com.apple.finder AppleShowAllFiles -bool TRUE \
