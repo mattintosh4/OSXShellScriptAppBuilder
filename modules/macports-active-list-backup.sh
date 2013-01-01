a088e2fea13588a6b79ba974ffda51e4e2c20cc5
mattintosh4 <mattintosh4@gmail.com>
2013-01-01 14:17:20 +0900

    .

--- modules/macports-active-list-backup.sh
+++ modules/macports-active-list-backup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash --login
+#!/bin/bash
 # MacPorts Active List Backup
 
 FILE=/tmp/macports-active-list_`date +%Y-%m-%d`.txt
-port installed active | awk 'NR >= 2 { sub(/^[^+]+/, "", $2); print $1, $2 }' > $FILE
+/opt/local/bin/port installed active | awk 'NR >= 2 { sub(/^[^+]+/, "", $2); print $1, $2 }' > $FILE
 open $FILE
